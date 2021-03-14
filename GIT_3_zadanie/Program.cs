df6f09ef42a0c587f8018356cc88d183fa5e7216
Krzysztof Pyszny <krzysztof.pyszny@edu.uekat.pl>
2021-03-14 09:38:46 +0100

    Initialize repository

---
+++ GIT_3_zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_3_zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
