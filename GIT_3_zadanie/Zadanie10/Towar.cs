9fe6f902932b005172345b782f94314c934a7a68
Krzysztof Pyszny <krzysztof.pyszny@edu.uekat.pl>
2021-03-14 11:07:21 +0100

    Add class Towar

---
+++ GIT_3_zadanie/Zadanie10/Towar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_3_zadanie
+{
+    class Towar
+    {
+        public string symbol;
+        public string nazwa;
+        public double cena;
+        public Towar(string symbol, string nazwa, double cena)
+        {
+            this.symbol = symbol;
+            this.nazwa = nazwa;
+            this.cena = cena;
+        }
+        public static void Wypisz(Towar[] towary)
+        {
+            foreach (var item in towary)
+            {
+                Console.WriteLine("{0,-15} {1,-20} {2,-10}",
+                    item.symbol, item.nazwa, item.cena);
+            }
+        }
+    }
+}
