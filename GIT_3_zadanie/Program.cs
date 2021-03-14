e871c33d9d75795e99e7b94b5fc65b4a8172bd6f
Krzysztof Pyszny <krzysztof.pyszny@edu.uekat.pl>
2021-03-14 10:42:45 +0100

    Adding main menu

--- GIT_3_zadanie/Program.cs
+++ GIT_3_zadanie/Program.cs
@@ -10,6 +10,16 @@ namespace GIT_3_zadanie
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Zadania programowanie współbieżne i rozproszone.\n");
+
+            int wybor;
+            do
+            {
+                Console.WriteLine("Wybierz zadanie 9 tasowanie obiektów wpisując: 9,\n" +
+                    "lub zadanie 10 sortowanie tablicy obiektow wpisując: 10,\n" +
+                    "aby wyjść z programu wpisz: 0");
+                wybor = int.Parse(Console.ReadLine());
+            } while (wybor != 0);
         }
     }
 }
---
+++ GIT_3_zadanie/Tresc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_3_zadanie
+{
+    class Tresc
+    {
+        public static string zad9 = "Napisz program z metodą generyczną służącą do tasowania tablicy obiektów.\n" +
+                "Tasowanie (mieszanie) jest operacją odwrotną względem sortowania,\n" +
+                "polega na losowej zamianie miejscami elementów danej kolekcji.\n" +
+                "Przetestuj metodę na dwóch tablicach, jednej typu int, a drugiej typu Karta.\n" +
+                "Zdefiniuj klasę Karta zawierającą dwa pola o odpowiednich typach: kolorKarty\n" +
+                "(trefl, pik, karo, kier) oraz nazwaKarty (np. walet, dama, król, as, dwójka, trójka, czwórka, itd.).\n" +
+                "Możesz wykorzystać prosty algorytm znany pod nazwą Fisher–Yates shuffle.\n" +
+                "W programie użyj metody Array.ForEach(T[], Action<T>) w celu wyświetlenia zawartości\n" +
+                "tablicy przed tasowaniem i po tasowaniu.Metoda ta dla podanej tablicy typu T wykonuje\n" +
+                "akcję zdefiniowaną poprzez delegata Action<T>.Do definicji akcji użyj wyrażenia lambda.";
+
+        public static string zad10 = "Napisz program z metodą generyczną służącą do sortowania tablicy obiektów\n" +
+            "dla dowolnej klasy(np.klasy Towar).Sortowanie wykonaj samodzielnie, stosując algorytm\n" +
+            "sortowania bąbelkowego.Zdefiniuj delegat wskazujący na metodę służącą do porównywania\n" +
+            "obiektów(według wybranego pola klasy lub jej właściwości).";
+    }
+}
