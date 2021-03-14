1539e65a00778809f5906c2cb19dfb9f42f2e92e
Pawel <pawel@mail.com>
2021-03-14 12:48:19 +0100

    Add class Karta

--- GIT_3_zadanie/Zadanie9.cs
+++ GIT_3_zadanie/Zadanie9.cs
@@ -8,6 +8,15 @@ namespace GIT_3_zadanie
 {
     class Zadanie9
     {
-        
+        public class Karta
+        {
+            public string kolorKarty;
+            public string nazwaKarty;
+            public Karta(string kolorKarty, string nazwaKarty)
+            {
+                this.kolorKarty = kolorKarty;
+                this.nazwaKarty = nazwaKarty;
+            }
+        }
     }
 }
